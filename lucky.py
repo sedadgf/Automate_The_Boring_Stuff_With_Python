c160e878a7ba4f1a1ff60e81c4eaeed526e80d9d
Seda <sedademir9311@gmail.com>
2019-03-27 14:55:42 +0000

    Update lucky.py

--- lucky.py
+++ lucky.py
@@ -1,5 +1,5 @@
-#This program searches products on Amazon website, than opens at most 5 products 
-#on new tabs 
+#This program searches products on Amazon website, than opens at most 5 products
+#on new tabs
 
 import requests, sys, webbrowser, bs4
 
@@ -10,7 +10,9 @@ else:
     product=input()
 
 url='https://amazon.co.uk/s?k=' + product
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) \
+            AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 \
+            Safari/537.36'}
 res = requests.get(url, headers=headers)
 print('Shopping...')
 res.raise_for_status()
@@ -23,9 +25,3 @@ linkElems=soup.find_all('a', class_='a-link-normal a-text-normal')
 numOpen=min(5, len(linkElems))
 for i in range(numOpen):
     webbrowser.open('https://www.amazon.co.uk' + linkElems[i].get('href'))
-    
-
-    
-    
-
-    
